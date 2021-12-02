23946a888d6901af5c1117ce0022fd7b25be5ae4
Scott Caulfield <scott.caulfield@student.manchester.ac.uk>
2021-12-02 16:42:29 +0000

    Sleeping bag heals player when used

--- src/games/stendhal/server/entity/item/SleepingBag.java
+++ src/games/stendhal/server/entity/item/SleepingBag.java
@@ -1,9 +1,12 @@
 package games.stendhal.server.entity.item;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
+//import games.stendhal.server.core.engine.SingletonRepository;
+//import games.stendhal.server.core.events.TurnNotifier;
 import games.stendhal.server.entity.RPEntity;
+//import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.status.EatStatus;
 
 /**
  * A sleeping bag that allows players to sleep, making them unable to move.
@@ -14,6 +17,14 @@ import games.stendhal.server.entity.RPEntity;
 
 public class SleepingBag extends Item {
 	
+	/**
+	 * Creates a new sleeping abg
+	 *
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
 	public SleepingBag(final String name, final String clazz, final String subclass, final Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 		setMenu("Use");
@@ -23,21 +34,18 @@ public class SleepingBag extends Item {
 		super(item);
 	}
 	
+	/**
+	 * Heals the player in increments of 3
+	 * 
+	 * @param user The player
+	 * 
+	 * @return true
+	 */
 	@Override
 	public boolean onUsed(final RPEntity user) {
 		
-		int maxHp = user.getBaseHP();
-		int currentHp  = user.getHP();
-		int healAmount = 1/100 * maxHp;
-		
-		while(currentHp < maxHp) {
-			user.heal(healAmount);
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+		EatStatus eat = new EatStatus(1000, 10, 3);
+		user.getStatusList().inflictStatus(eat, user);
 		
 		return true;
 	}
